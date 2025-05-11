d7a6ff691bcb8de436a070368c66d7c0452bca5e
Roman <roman.it.kirov@yandex.ru>
2025-05-11 22:57:49 +0300

    Settings UI
    Menu->Settings class

--- Source/PingPong/UI/Settings.cpp
+++ Source/PingPong/UI/Settings.cpp
@@ -3,3 +3,53 @@
 
 #include "Settings.h"
 
+#include "MenuAudio.h"
+#include "Components/Button.h"
+#include "Components/WidgetSwitcher.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+
+void USettings::NativeConstruct()
+{	
+	if (AudioSettingsSubClass)
+	{
+		AudioSettingsWidget = CreateWidget<UUserWidget>(GetWorld(), AudioSettingsSubClass);
+		WidgetSwitcher->AddChild(AudioSettingsWidget);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No AudioSettingsSubClass in Settings Widget"));
+		UKismetSystemLibrary::QuitGame(GetWorld(), GetWorld()->GetFirstPlayerController(), EQuitPreference::Quit, true);
+	}
+
+	if (VideoSettingsSubClass)
+	{
+		VideoSettingsWidget = CreateWidget<UUserWidget>(GetWorld(), VideoSettingsSubClass); // ✅ Correct class
+		WidgetSwitcher->AddChild(VideoSettingsWidget);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No VideoSettingsSubClass in Settings Widget"));
+		UKismetSystemLibrary::QuitGame(GetWorld(), GetWorld()->GetFirstPlayerController(), EQuitPreference::Quit, true);
+	}
+
+	Sound->OnPressed.AddUniqueDynamic(this, &USettings::OnSoundBtnPushed);
+	Video->OnPressed.AddUniqueDynamic(this, &USettings::OnVideoBtnPushed);
+	Super::NativeConstruct();
+}
+
+void USettings::OnVideoBtnPushed()
+{
+	if (WidgetSwitcher && VideoSettingsWidget)
+	{
+		WidgetSwitcher->SetActiveWidget(VideoSettingsWidget);
+	}
+}
+
+void USettings::OnSoundBtnPushed()
+{
+	if (WidgetSwitcher && AudioSettingsWidget)
+	{
+		WidgetSwitcher->SetActiveWidget(AudioSettingsWidget);
+	}
+}
--- Source/PingPong/UI/Settings.h
+++ Source/PingPong/UI/Settings.h
@@ -6,12 +6,44 @@
 #include "Blueprint/UserWidget.h"
 #include "Settings.generated.h"
 
+class UWidgetSwitcher;
+class UMenuAudio;
+class UGridPanel;
+class UButton;
+class UComboBoxString;
+class UVerticalBox;
+class UHorizontalBox;
 /**
  * 
  */
+
 UCLASS()
 class PINGPONG_API USettings : public UUserWidget
 {
 	GENERATED_BODY()
+protected:
+	virtual void NativeConstruct() override;
+	UPROPERTY(meta = (BindWidget))
+	UGridPanel* GridPanel;
+	UPROPERTY(meta = (BindWidget))
+	UButton* Sound;	
+	UPROPERTY(meta = (BindWidget))
+	UButton* Video;
+public:
+	UFUNCTION()
+	void OnVideoBtnPushed();
+	UFUNCTION()
+	void OnSoundBtnPushed();
+protected:	
+	UPROPERTY(meta = (BindWidget))
+	UWidgetSwitcher* WidgetSwitcher;
 	
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UUserWidget> AudioSettingsSubClass;
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UUserWidget> VideoSettingsSubClass;
+	UPROPERTY()
+	UUserWidget* AudioSettingsWidget;
+	UPROPERTY()
+	UUserWidget* VideoSettingsWidget;
 };
