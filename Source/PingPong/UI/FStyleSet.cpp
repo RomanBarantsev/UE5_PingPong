2322d3c6ff6e7e482d71e671dee1c20514a28800
Roman <roman.it.kirov@yandex.ru>
2025-05-16 13:33:43 +0300

    FStyleSet class
    for Slate styles.

---
+++ Source/PingPong/UI/FStyleSet.cpp
@@ -0,0 +1,39 @@
+﻿#include "FStyleSet.h"
+
+#include "Slate/SlateGameResources.h"
+#include "Styling/SlateStyleRegistry.h"
+
+TSharedPtr<ISlateStyle> FindStyle(const FString& ScopeToDirectory)
+{
+	TSharedPtr<ISlateStyle> Style = FSlateGameResources::New(FName("StyleSet"),
+	ScopeToDirectory, ScopeToDirectory);
+	return Style;
+}
+
+TSharedPtr<ISlateStyle> FStyleSet::StylePtr = nullptr;
+
+void FStyleSet::Initialize()
+{
+	Shutdown();
+	StylePtr = FindStyle("/Game/PingPong/UI/"	);
+	FSlateStyleRegistry::RegisterSlateStyle(*StylePtr);
+}
+
+void FStyleSet::Shutdown()
+{
+	if (StylePtr.IsValid())
+	{
+		FSlateStyleRegistry::UnRegisterSlateStyle(*StylePtr);
+		ensure(StylePtr.IsUnique());
+		StylePtr.Reset();
+	}
+}
+
+const ISlateStyle& FStyleSet::Get()
+{
+	if (!StylePtr.IsValid())
+	{
+		Initialize();
+	}
+	return *StylePtr;
+}
