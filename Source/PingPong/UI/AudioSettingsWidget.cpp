c4269358d5bb8b2de5ee3331bba7005a1f3687ca
Roman <roman.it.kirov@yandex.ru>
2025-06-06 17:42:14 +0300

    new AudioSettingsWidget
    new class for audio settings widget.

---
+++ Source/PingPong/UI/AudioSettingsWidget.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AudioSettingsWidget.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundClass.h"
+#include "Sound/SoundMix.h"
+
+void UAudioSettingsWidget::OnVolumeChanged(float X)
+{
+	UGameplayStatics::SetSoundMixClassOverride(
+		this,
+		MasterSoundMix,
+		MasterSound,
+		X,
+		1.0f,  // pitch
+		0.0f   // fade-in time
+	);
+	UGameplayStatics::PushSoundMixModifier(this, MasterSoundMix);
+}
+
+void UAudioSettingsWidget::NativeConstruct()
+{
+	check(MasterSound);
+	check(MasterSoundMix);
+	VolumeSlider->OnValueChanged.AddDynamic(this,&ThisClass::OnVolumeChanged);
+	Super::NativeConstruct();
+}
---
+++ Source/PingPong/UI/AudioSettingsWidget.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/Slider.h"
+#include "AudioSettingsWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PINGPONG_API UAudioSettingsWidget : public UUserWidget
+{
+	GENERATED_BODY()
+protected:
+	UFUNCTION()
+	void OnVolumeChanged(float X);
+	virtual void NativeConstruct() override;
+	UPROPERTY(EditAnywhere)
+	USoundClass* MasterSound;
+	UPROPERTY(EditAnywhere)
+	USoundMix* MasterSoundMix;
+	UPROPERTY(BlueprintReadWrite,meta=(BindWidget))
+	TObjectPtr<USlider> VolumeSlider;	
+};
