1eef3e5d2f76dc63d3ba13b094de74bd3ce93ccd
Roman <roman.it.kirov@yandex.ru>
2025-06-10 22:48:26 +0300

    new class - GameSettings
    right now just have name editable text in it.

---
+++ Source/PingPong/UI/GameSettings.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameSettings.h"
+
+#include "Kismet/GameplayStatics.h"
+
+void UGameSettings::TextCommited(const FText& Text, ETextCommit::Type CommitMethod)
+{
+	APlayerController* PC = GetOwningPlayer();
+	if (!PC) return;
+	APlayerState* PS = PC->PlayerState;
+	if (PS)
+	{
+		PS->SetPlayerName(Text.ToString());
+	}	
+}
+
+void UGameSettings::NativeConstruct()
+{
+	Nickname->OnTextCommitted.AddDynamic(this,&ThisClass::TextCommited);
+	Super::NativeConstruct();
+}
---
+++ Source/PingPong/UI/GameSettings.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/EditableText.h"
+#include "PingPong/PlayerStates/PingPongPlayerState.h"
+#include "GameSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PINGPONG_API UGameSettings : public UUserWidget
+{
+	GENERATED_BODY()
+	UPROPERTY(meta = (BindWidget))
+	UEditableText* Nickname;
+	UPROPERTY()
+	APingPongPlayerState* PlayerState;
+protected:
+	UFUNCTION()
+	void TextCommited(const FText& Text, ETextCommit::Type CommitMethod);
+	virtual void NativeConstruct() override;
+};
