d24924a17e4ff65f948c6e3f6ad7b7ba8d31f7aa
Roman <roman.it.kirov@yandex.ru>
2025-06-13 23:59:45 +0300

    class PlayerScore
    class for creating dynamicly at the table of players

---
+++ Source/PingPong/UI/PlayerScore.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerScore.h"
+
+#include <string>
+
+#include "Kismet/KismetSystemLibrary.h"
+
+void UPlayerScore::NativeConstruct()
+{
+	Super::NativeConstruct();
+}
+
+void UPlayerScore::SetPlayerName(FString Name)
+{
+	PlayerName->SetText(FText::FromString(Name));
+}
+
+void UPlayerScore::SetPlayerScore(int Score)
+{	
+	ScoreText->SetText(FText::AsNumber(Score));
+}
---
+++ Source/PingPong/UI/PlayerScore.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/HorizontalBox.h"
+#include "Components/TextBlock.h"
+#include "PlayerScore.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PINGPONG_API UPlayerScore : public UUserWidget
+{
+	UPROPERTY(Meta = (BindWidget))
+	UTextBlock* ScoreText;
+	UPROPERTY(Meta = (BindWidget))
+	UTextBlock* PlayerName;
+	UPROPERTY(Meta = (BindWidget))
+	UHorizontalBox* HorizontalBox;
+	GENERATED_BODY()
+	virtual void NativeConstruct() override;
+	public:
+	UFUNCTION()
+	void SetPlayerName(FString Name);
+	UFUNCTION()
+	void SetPlayerScore(int Score);
+};
