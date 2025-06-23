18e709af2cdc4f8f2a5027cd987118e0aa72f547
Roman <roman.it.kirov@yandex.ru>
2025-06-23 15:49:10 +0300

    GameUserSettings
    just simple inherit class with one value

---
+++ Source/PingPong/Pong_GameUserSettings.cpp
@@ -0,0 +1,7 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pong_GameUserSettings.h"
+
+#include "GameFramework/PlayerState.h"
+#include "Kismet/GameplayStatics.h"
---
+++ Source/PingPong/Pong_GameUserSettings.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameUserSettings.h"
+#include "Pong_GameUserSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PINGPONG_API UPong_GameUserSettings : public UGameUserSettings
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(Config)
+	FString PlayerName;	
+};
