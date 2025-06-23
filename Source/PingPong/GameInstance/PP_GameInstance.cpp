54850bb6994f7013a410893d0231534cd20c1963
Roman <roman.it.kirov@yandex.ru>
2025-06-23 15:50:57 +0300

    new class Pong_GameInstance
    class which loading default config from default GameUserSettings class.

---
+++ Source/PingPong/GameInstance/PP_GameInstance.cpp
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PP_GameInstance.h"
+
+#include "GameFramework/GameUserSettings.h"
+#include "GameFramework/PlayerState.h"
+#include "PingPong/Pong_GameUserSettings.h"
+
+class UPong_GameSettings;
+
+void UPP_GameInstance::Init()
+{
+	/*if (GEngine)
+	{
+		GEngine->GameUserSettings = NewObject<UPong_GameUserSettings>();
+		GEngine->GameUserSettings->LoadSettings(true);
+	}*/
+	UGameUserSettings* Settings = GEngine->GameUserSettings;
+	if (Settings)
+	{
+		Settings->LoadConfig();	
+		Pong_Settings = Cast<UPong_GameUserSettings>(Settings);
+		check(Pong_Settings);	
+	}
+	Super::Init();
+}
+
+void UPP_GameInstance::Shutdown()
+{		
+	Super::Shutdown();
+}
+
+void UPP_GameInstance::LoadAudioSettings()
+{
+	
+}
+
+void UPP_GameInstance::LoadVideoSettings()
+{
+	
+}
---
+++ Source/PingPong/GameInstance/PP_GameInstance.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "PP_GameInstance.generated.h"
+
+class UPong_GameUserSettings;
+class UPong_GameSettings;
+/**
+ * 
+ */
+UCLASS()
+class PINGPONG_API UPP_GameInstance : public UGameInstance
+{
+	virtual void Init() override;
+	virtual void Shutdown() override;
+	UPROPERTY()
+	UPong_GameUserSettings* Pong_Settings;
+	UPROPERTY()
+	APlayerState* PlayerState;
+public:	
+	void LoadAudioSettings();
+	void LoadVideoSettings();	
+	GENERATED_BODY()
+};
