fe4e07ce4e8185d70b9bb88b01be2badc80a7391
Roman <roman.it.kirov@yandex.ru>
2025-06-24 16:28:14 +0300

    updating Pong_GameUserSettings
    Saving and loading audio settings fine.

--- Source/PingPong/Pong_GameUserSettings.h
+++ Source/PingPong/Pong_GameUserSettings.h
@@ -6,6 +6,7 @@
 #include "GameFramework/GameUserSettings.h"
 #include "Pong_GameUserSettings.generated.h"
 
+class UAudioVolumeSettingsDataAsset;
 /**
  * 
  */
@@ -13,7 +14,21 @@ UCLASS()
 class PINGPONG_API UPong_GameUserSettings : public UGameUserSettings
 {
 	GENERATED_BODY()
+	UPong_GameUserSettings();
+	~UPong_GameUserSettings();
 public:
 	UPROPERTY(Config)
-	FString PlayerName;	
+	FString PlayerName;
+	UPROPERTY()
+	UAudioVolumeSettingsDataAsset* VolumeData;
+	UPROPERTY(Config)
+	float MasterVolume;
+	UPROPERTY(Config)
+	float MusicVolume;
+	UPROPERTY(Config)
+	float SFXVolume;
+	virtual void LoadSettings(bool bForceReload = false) override;
+	void SaveAudioSettings(USoundClass* SoundClass, float Value);
+	void LoadAuidoSettings();
+	
 };
