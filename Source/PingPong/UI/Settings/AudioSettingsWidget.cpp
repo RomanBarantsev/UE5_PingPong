fd6fd5c70108b8d611b0b17e0dff1f3e01b3740b
Roman <roman.it.kirov@yandex.ru>
2025-06-24 16:27:07 +0300

    update AudioSettings
    Getting all values from DataAsset.

--- Source/PingPong/UI/Settings/AudioSettingsWidget.cpp
+++ Source/PingPong/UI/Settings/AudioSettingsWidget.cpp
@@ -3,6 +3,8 @@
 
 #include "AudioSettingsWidget.h"
 #include "Kismet/GameplayStatics.h"
+#include "PingPong/Pong_GameUserSettings.h"
+#include "PingPong/Settings/Audio/AudioVolumeSettingsDataAsset.h"
 #include "PingPong/UI/SlateClasses/PPSlider.h"
 #include "Sound/SoundClass.h"
 
@@ -10,24 +12,46 @@ void UAudioSettingsWidget::VolumeChanged(const float X, USoundClass* sClass) con
 {
 	UGameplayStatics::SetSoundMixClassOverride(
 		this,
-		MasterSoundMix,
+		VolumeData->SoundMix,
 		sClass,
 		X,
 		1.0f,  // pitch
 		0.0f   // fade-in time
 	);	
-	UGameplayStatics::PushSoundMixModifier(this, MasterSoundMix);
-	
+	UGameplayStatics::PushSoundMixModifier(this, VolumeData->SoundMix);
+	Pong_GameUserSettings->SaveAudioSettings(sClass, X);
 }
 
 void UAudioSettingsWidget::NativeConstruct()
 {
-	MasterSlider->AssociatedClass=MasterSound;
-	VoiceSlider->AssociatedClass=EnvironmentSound;
-	MusicSlider->AssociatedClass=MusicSound;
+	Pong_GameUserSettings = Cast<UPong_GameUserSettings>(GEngine->GetGameUserSettings());
+	if (Pong_GameUserSettings)
+	{
+		Pong_GameUserSettings->LoadAuidoSettings();
+	}
+	check(Pong_GameUserSettings);
+	if (!VolumeData)
+	{
+		VolumeData = Cast<UAudioVolumeSettingsDataAsset>(StaticLoadObject(
+			UAudioVolumeSettingsDataAsset::StaticClass(),
+			nullptr,
+			TEXT("/Game/PingPong/Sounds/AudioDataAsset.AudioDataAsset")  // path to your asset
+		));
+	}
+	check(VolumeData); 
+	if (!VolumeData) return;
+	MasterSlider->SetValue(VolumeData->MasterVolume);
+	VoiceSlider->SetValue(VolumeData->SFXVolume);
+	MusicSlider->SetValue(VolumeData->MusicVolume);
+	VolumeChanged(VolumeData->MasterVolume,VolumeData->MasterClass);
+	VolumeChanged(VolumeData->SFXVolume,VolumeData->SFXClass);
+	VolumeChanged(VolumeData->MusicVolume,VolumeData->MusicClass);
+	MasterSlider->AssociatedClass=VolumeData->MasterClass;
+	VoiceSlider->AssociatedClass=VolumeData->SFXClass;
+	MusicSlider->AssociatedClass=VolumeData->MusicClass;
 	Sliders.Emplace(MasterSlider);
 	Sliders.Emplace(VoiceSlider);
-	Sliders.Emplace(MusicSlider);
+	Sliders.Emplace(MusicSlider);	
 	for (const auto Slider : Sliders)
 	{
 		if (!Slider || !Slider->AssociatedClass) continue;
