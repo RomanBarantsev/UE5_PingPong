f00f80488752ab2bdcc39ead4a8ab1ec498e1cdf
Roman <roman.it.kirov@yandex.ru>
2025-08-04 17:01:03 +0300

    ClassingPong Player Controller

---
+++ Source/PingPong/PlayerControllers/ClassicPongPlayerController.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ClassicPongPlayerController.h"
+
+void AClassicPongPlayerController::RotatePlatform(float AxisValue)
+{
+	//Super::RotatePlatform(AxisValue);
+}
+
+void AClassicPongPlayerController::MoveForward(float AxisValue)
+{
+	//Super::MoveForward(AxisValue);
+}
+
+void AClassicPongPlayerController::Fire()
+{
+	//Super::Fire();
+}
