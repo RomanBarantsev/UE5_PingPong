9a7f0b0a06679bdd6531d012c597a216135592cc
Roman <roman.it.kirov@yandex.ru>
2025-08-04 15:17:19 +0300

    ClassinPongGameMode

---
+++ Source/PingPong/GameModes/PongClassicGameMode.cpp
@@ -0,0 +1,11 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PongClassicGameMode.h"
+
+#include "PingPong/Pawns/ClassicPongPawn.h"
+
+APongClassicGameMode::APongClassicGameMode()
+{
+	DefaultPawnClass = AClassicPongPawn::StaticClass();
+}
---
+++ Source/PingPong/GameModes/PongClassicGameMode.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PongGameMode.h"
+#include "PongClassicGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PINGPONG_API APongClassicGameMode : public APongGameMode
+{
+	GENERATED_BODY()
+	APongClassicGameMode();
+};
