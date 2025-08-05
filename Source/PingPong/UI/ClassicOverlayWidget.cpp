dfd6d29bc03986be1929d3ed55fc20f2df3eaa25
Roman <roman.it.kirov@yandex.ru>
2025-08-05 17:29:37 +0300

    New Overlay widget for Classic Pong map
    overlay for classic pong

---
+++ Source/PingPong/UI/ClassicOverlayWidget.cpp
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ClassicOverlayWidget.h"
+
+void UClassicOverlayWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	//PlayersScoreTable->SetVisibility(ESlateVisibility::Collapsed);
+	ShotButton->SetVisibility(ESlateVisibility::Collapsed);
+	BallCost->SetVisibility(ESlateVisibility::Collapsed);
+}
---
+++ Source/PingPong/UI/ClassicOverlayWidget.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "OverlayWidget.h"
+#include "ClassicOverlayWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PINGPONG_API UClassicOverlayWidget : public UOverlayWidget
+{
+	GENERATED_BODY()
+	virtual void NativeConstruct() override;
+};
