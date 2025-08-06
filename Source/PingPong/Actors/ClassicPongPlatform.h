98a2837073feae246255e70a90c2cc76f4722dd1
Roman <roman.it.kirov@yandex.ru>
2025-08-06 21:15:36 +0300

    new Platform class for more classic movement Atari type.

---
+++ Source/PingPong/Actors/ClassicPongPlatform.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PongPlatform.h"
+#include "ClassicPongPlatform.generated.h"
+
+UCLASS()
+class PINGPONG_API AClassicPongPlatform : public APongPlatform
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AClassicPongPlatform();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
