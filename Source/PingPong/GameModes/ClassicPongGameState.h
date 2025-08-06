bb834d0a1d25b263cd23a2fe62a52bd035b1d8e9
Roman <roman.it.kirov@yandex.ru>
2025-08-06 23:32:21 +0300

    new GameState for Classic Pong

---
+++ Source/PingPong/GameModes/ClassicPongGameState.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PingPong/GameStates/PongGameState.h"
+#include "ClassicPongGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PINGPONG_API AClassicPongGameState : public APongGameState
+{
+	GENERATED_BODY()
+	AClassicPongGameState();
+};
