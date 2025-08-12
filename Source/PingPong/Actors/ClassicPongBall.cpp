70ec737f9437c53034256e8c55234e889400ccfd
Roman <roman.it.kirov@yandex.ru>
2025-08-12 23:45:43 +0300

    Update ClassicPongBall.cpp
    fixing for compiling

--- Source/PingPong/Actors/ClassicPongBall.cpp
+++ Source/PingPong/Actors/ClassicPongBall.cpp
@@ -4,7 +4,6 @@
 #include "ClassicPongBall.h"
 
 #include "PongGoal.h"
-#include "AssetTypeActions/AssetDefinition_SoundBase.h"
 #include "Components/AudioComponent.h"
 #include "PingPong/PlayerStates/PongPlayerState.h"
 
